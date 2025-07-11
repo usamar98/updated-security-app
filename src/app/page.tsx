bbdd6bbd3c6e53512d3d1e9bb5a8fb4d9dae20e7
usamar98 <connecttousama@gmail.com>
2025-07-11 07:59:17 +0500

    Fix Lenis configuration TypeScript errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,6 @@ export default function Home() {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
-      gestureDirection: 'vertical',
       smooth: true,
       mouseMultiplier: 1,
       smoothTouch: false,
