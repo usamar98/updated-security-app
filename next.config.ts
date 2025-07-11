dfb622cdef30c5a492a80dc007808bf6d78e37ad
usamar98 <connecttousama@gmail.com>
2025-07-11 07:55:07 +0500

    Fix next.config.ts duplicate export issue

--- next.config.ts
+++ next.config.ts
@@ -10,13 +10,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
-};
-
-export default nextConfig;
